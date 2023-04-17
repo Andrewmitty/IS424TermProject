659d579beb04741168ce5fd71e9d06b30d720080
Andrew Mitty <61165178+Andrewmitty@users.noreply.github.com>
2023-04-16 21:48:41 -0500

    Menu Loads from DB

--- menu.js
+++ menu.js
@@ -12,4 +12,24 @@ function renderMenu(doc) {
     var desc = doc.data().description
     var img = doc.data().image
     var avail = doc.data().available
+
+    if (cat == "Appetizers"){
+        var div = document.getElementById("tileParApps");
+    }
+    else if (cat == "Mains"){
+        var div = document.getElementById("tileParMain");
+    }
+    else if (cat == "Dessert"){
+        var div = document.getElementById("tileParDesserts");
+    }
+    else if (cat == "Beverages"){
+        var div = document.getElementById("tileParDrinks");
+    }
+    else if (cat == "Cakes"){
+        var div = document.getElementById("tileParCakes");
+    }
+    else if (cat == "Cupcakes"){
+        var div = document.getElementById("tileParCupcakes");
+    }
+    div.innerHTML += "<div class='tile is-child box is-4'>  <p class='title'>" + name + "</p>  <p class='subtitle'>" + price + "</p>  <p class='subtitle'>" + desc + "</p>  <p class='subtitle'>" + cat + "</p>  <figure class='image is-4by3'>    <img src='" + img + "'>  </figure></div>"
 }
